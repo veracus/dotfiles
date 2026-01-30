ebe05dfdbd2c4d09eabbec71e13a3a1ed9637f58
Jackson Sadowski <me@jacksonsadowski.com>
2026-01-30 17:01:40 -0500

    tab titlestring

--- init.lua
+++ init.lua
@@ -110,6 +110,10 @@ vim.o.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
+-- Set terminal title so Ghostty shows a useful tab name.
+vim.o.title = true
+vim.o.titlestring = "nvim: %{fnamemodify(getcwd(), ':~')}"
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
